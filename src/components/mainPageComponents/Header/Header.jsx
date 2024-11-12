927361fd0fec599b8dfab75acd016032c85f1c1d
bohdan.snopok <snopokbogdan3@gmail.com>
2024-11-12 22:34:32 +0200

    first commit

---
+++ src/components/mainPageComponents/Header/Header.jsx
@@ -0,0 +1,18 @@
+import './Header.css';
+import logo from '../../images/logo.png'
+import cart from '../../images/cart.svg'
+
+export function Header() {
+    return (
+        <header className="header">
+            <div className="container">
+                <div className="logo">
+                    <img src={logo} alt=""/>
+                    footsnop
+                </div>
+
+                <a href="#"><img src={cart} alt="" className="cart"/></a>
+            </div>
+        </header>
+    );
+}
